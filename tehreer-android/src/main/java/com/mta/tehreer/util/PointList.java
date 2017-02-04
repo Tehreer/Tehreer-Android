63e047b1afab4fae55afc7ac80cc604713c738d9
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-02-04 23:33:28 +0500

    [lib] Added interface for point list...

---
+++ tehreer-android/src/main/java/com/mta/tehreer/util/PointList.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2017 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.util;
+
+public interface PointList extends PrimitiveList {
+
+    int size();
+
+    float getX(int index);
+    float getY(int index);
+
+    void setX(int index, float value);
+    void setY(int index, float value);
+
+    float[] toArray(float scale);
+}
