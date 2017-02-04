66631d2d0df2e824bc24179e80f55ef41686a94b
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-02-04 14:35:14 +0500

    [lib] Added class for raw Int32 list...

---
+++ tehreer-android/src/main/java/com/mta/tehreer/internal/util/RawInt32List.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2017 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.internal.util;
+
+import com.mta.tehreer.internal.Raw;
+import com.mta.tehreer.util.IntList;
+
+public class RawInt32List implements IntList {
+
+    private final long pointer;
+    private final int size;
+
+    public RawInt32List(long pointer, int size) {
+        this.pointer = pointer;
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return Primitives.equals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Primitives.hashCode(this);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+
+        return Raw.getInt32(pointer, index);
+    }
+
+    @Override
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+
+        Raw.putInt32(pointer, index, value);
+    }
+
+    @Override
+    public int[] toArray() {
+        return Raw.toInt32Array(pointer, size);
+    }
+
+    @Override
+    public String toString() {
+        return Description.forIntList(this);
+    }
+}
