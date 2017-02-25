903d612eb85173ed6a8226f15b360a5b850dc01d
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-02-25 17:20:55 +0500

    [lib] Updated convert utility class...

--- tehreer-android/src/main/java/com/mta/tehreer/internal/util/Convert.java
+++ tehreer-android/src/main/java/com/mta/tehreer/internal/util/Convert.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Muhammad Tayyab Akram
+ * Copyright (C) 2017 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,55 +16,31 @@
 
 package com.mta.tehreer.internal.util;
 
-import com.mta.tehreer.text.TextDirection;
-import com.mta.tehreer.text.TextMode;
-
-import java.util.EnumMap;
+import com.mta.tehreer.opentype.ShapingDirection;
+import com.mta.tehreer.opentype.ShapingOrder;
 
 public class Convert {
 
-    private static final EnumMap<TextDirection, Integer> JAVA_TEXT_DIRECTION_MAP;
-    private static final TextDirection[] NATIVE_TEXT_DIRECTION_MAP;
-    private static final int NATIVE_TEXT_DIRECTION_OFFSET = 1;
-
-    private static final EnumMap<TextMode, Integer> JAVA_TEXT_MODE_MAP;
-    private static final TextMode[] NATIVE_TEXT_MODE_MAP;
+    private static final ShapingDirection[] NATIVE_TEXT_DIRECTION_MAP;
+    private static final ShapingOrder[] NATIVE_TEXT_MODE_MAP;
 
     static {
-        JAVA_TEXT_DIRECTION_MAP = new EnumMap<>(TextDirection.class);
-        JAVA_TEXT_DIRECTION_MAP.put(TextDirection.DEFAULT, 0 - NATIVE_TEXT_DIRECTION_OFFSET);
-        JAVA_TEXT_DIRECTION_MAP.put(TextDirection.LEFT_TO_RIGHT, 1 - NATIVE_TEXT_DIRECTION_OFFSET);
-        JAVA_TEXT_DIRECTION_MAP.put(TextDirection.RIGHT_TO_LEFT, 2 - NATIVE_TEXT_DIRECTION_OFFSET);
-
-        NATIVE_TEXT_DIRECTION_MAP = new TextDirection[] {
-                TextDirection.DEFAULT,
-                TextDirection.LEFT_TO_RIGHT,
-                TextDirection.RIGHT_TO_LEFT
+        NATIVE_TEXT_DIRECTION_MAP = new ShapingDirection[] {
+                ShapingDirection.LEFT_TO_RIGHT,
+                ShapingDirection.RIGHT_TO_LEFT
         };
 
-        JAVA_TEXT_MODE_MAP = new EnumMap<>(TextMode.class);
-        JAVA_TEXT_MODE_MAP.put(TextMode.FORWARD, 0);
-        JAVA_TEXT_MODE_MAP.put(TextMode.BACKWARD, 1);
-
-        NATIVE_TEXT_MODE_MAP = new TextMode[] {
-                TextMode.FORWARD,
-                TextMode.BACKWARD
+        NATIVE_TEXT_MODE_MAP = new ShapingOrder[] {
+                ShapingOrder.FORWARD,
+                ShapingOrder.BACKWARD
         };
     }
 
-    public static int toNativeTextDirection(TextDirection javaTextDirection) {
-        return JAVA_TEXT_DIRECTION_MAP.get(javaTextDirection);
+    public static ShapingDirection toJavaTextDirection(int nativeTextDirection) {
+        return NATIVE_TEXT_DIRECTION_MAP[nativeTextDirection];
     }
 
-    public static TextDirection toJavaTextDirection(int nativeTextDirection) {
-        return NATIVE_TEXT_DIRECTION_MAP[nativeTextDirection + NATIVE_TEXT_DIRECTION_OFFSET];
-    }
-
-    public static int toNativeTextMode(TextMode javaTextMode) {
-        return JAVA_TEXT_MODE_MAP.get(javaTextMode);
-    }
-
-    public static TextMode toJavaTextMode(int nativeTextMode) {
+    public static ShapingOrder toJavaTextMode(int nativeTextMode) {
         return NATIVE_TEXT_MODE_MAP[nativeTextMode];
     }
 
