12a9d8281a5a0c3197391538eff86a80676a4593
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-04-01 23:22:48 +0500

    [lib] Added an internal interface for reading SFNT tables...

---
+++ tehreer-android/src/main/java/com/mta/tehreer/opentype/SfntTable.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.opentype;
+
+interface SfntTable {
+    byte[] readBytes(int offset, int count);
+
+    short readInt16(int offset);
+    int readInt32(int offset);
+
+    int readUInt16(int offset);
+    long readUInt32(int offset);
+
+    long readInt64(int offset);
+}
