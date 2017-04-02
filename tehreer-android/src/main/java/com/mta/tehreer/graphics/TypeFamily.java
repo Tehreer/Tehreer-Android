251e2d7e125d3749f7425f023ea5c675d4a9300d
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2017-04-02 12:42:52 +0500

    [lib] Added a class to represent type family...

---
+++ tehreer-android/src/main/java/com/mta/tehreer/graphics/TypeFamily.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.graphics;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TypeFamily {
+
+    final String familyName;
+    final List<Typeface> typefaces;
+
+    TypeFamily(String familyName, List<Typeface> typefaces) {
+        this.familyName = familyName;
+        this.typefaces = typefaces;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public List<Typeface> getTypefaces() {
+        return Collections.unmodifiableList(typefaces);
+    }
+}
