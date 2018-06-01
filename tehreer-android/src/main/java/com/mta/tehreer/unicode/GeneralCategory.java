5c51db871a314c245abbd5f3b6b6422a3b682f95
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-06-02 02:35:54 +0500

    Added enum for unicode general category values

---
+++ tehreer-android/src/main/java/com/mta/tehreer/unicode/GeneralCategory.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.unicode;
+
+public enum GeneralCategory {
+    UPPERCASE_LETTER,
+    LOWERCASE_LETTER,
+    TITLECASE_LETTER,
+    MODIFIER_LETTER,
+    OTHER_LETTER,
+    NONSPACING_MARK,
+    SPACING_MARK,
+    ENCLOSING_MARK,
+    DECIMAL_NUMBER,
+    LETTER_NUMBER,
+    OTHER_NUMBER,
+    CONNECTOR_PUNCTUATION,
+    DASH_PUNCTUATION,
+    OPEN_PUNCTUATION,
+    CLOSE_PUNCTUATION,
+    INITIAL_PUNCTUATION,
+    FINAL_PUNCTUATION,
+    OTHER_PUNCTUATION,
+    MATH_SYMBOL,
+    CURRENCY_SYMBOL,
+    MODIFIER_SYMBOL,
+    OTHER_SYMBOL,
+    SPACE_SEPARATOR,
+    LINE_SEPARATOR,
+    PARAGRAPH_SEPARATOR,
+    CONTROL,
+    FORMAT,
+    SURROGATE,
+    PRIVATE_USE,
+    UNASSIGNED;
+
+    private static final GeneralCategory[] all = GeneralCategory.values();
+
+    static GeneralCategory valueOf(int nValue) {
+        if (nValue > 0 && nValue < all.length) {
+            return all[nValue - 1];
+        }
+
+        return null;
+    }
+}
