9fa3107aa6a5f570003c695a6642dfedfd0333e8
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-06-24 19:01:41 +0500

    [lib] Renamed static pointer size field

--- tehreer-android/src/main/java/com/mta/tehreer/internal/Raw.java
+++ tehreer-android/src/main/java/com/mta/tehreer/internal/Raw.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Muhammad Tayyab Akram
+ * Copyright (C) 2017-2018 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ public class Raw {
         JniBridge.loadLibrary();
     }
 
-    public static final int BYTES_IN_SIZE_TYPE = bytesInSizeType();
+    public static final int POINTER_SIZE = bytesInSizeType();
 
     private static native int bytesInSizeType();
 
