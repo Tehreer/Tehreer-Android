00dae027c8f7f939194714618ce3bffaf27f1e3d
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-07-11 22:22:34 +0500

    [lib] Made glyph strike data class final

--- tehreer-android/src/main/java/com/mta/tehreer/graphics/GlyphStrike.java
+++ tehreer-android/src/main/java/com/mta/tehreer/graphics/GlyphStrike.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Muhammad Tayyab Akram
+ * Copyright (C) 2016-2018 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,7 @@
 
 package com.mta.tehreer.graphics;
 
-class GlyphStrike implements Cloneable {
-
+final class GlyphStrike implements Cloneable {
     public Typeface typeface;
     public int pixelWidth;      // 26.6 fixed-point value.
     public int pixelHeight;     // 26.6 fixed-point value.
