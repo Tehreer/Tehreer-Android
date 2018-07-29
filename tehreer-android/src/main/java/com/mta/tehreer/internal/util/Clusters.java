9222efe2c6c9eed58a94e5b556a1e3fff96b5512
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-07-29 13:03:18 +0500

    [lib] Added primitive array methods in clusters utility class

--- tehreer-android/src/main/java/com/mta/tehreer/internal/util/Clusters.java
+++ tehreer-android/src/main/java/com/mta/tehreer/internal/util/Clusters.java
@@ -35,13 +35,40 @@ public final class Clusters {
         return clusterMap.get(arrayIndex);
     }
 
+    private static int forwardGlyphIndex(int[] clusterMap, int arrayIndex, int glyphCount) {
+        int common = clusterMap[arrayIndex];
+        int length = clusterMap.length;
+
+        for (int i = arrayIndex + 1; i < length; i++) {
+            int mapping = clusterMap[i];
+            if (mapping != common) {
+                return mapping - 1;
+            }
+        }
+
+        return glyphCount - 1;
+    }
+
     private static int forwardGlyphIndex(IntList clusterMap, int arrayIndex, int glyphCount) {
-        int leading = clusterMap.get(arrayIndex);
+        int common = clusterMap.get(arrayIndex);
         int length = clusterMap.size();
 
         for (int i = arrayIndex + 1; i < length; i++) {
             int mapping = clusterMap.get(i);
-            if (mapping != leading) {
+            if (mapping != common) {
+                return mapping - 1;
+            }
+        }
+
+        return glyphCount - 1;
+    }
+
+    private static int backwardGlyphIndex(int[] clusterMap, int arrayIndex, int glyphCount) {
+        int common = clusterMap[arrayIndex];
+
+        for (int i = arrayIndex - 1; i >= 0; i--) {
+            int mapping = clusterMap[i];
+            if (mapping != common) {
                 return mapping - 1;
             }
         }
@@ -50,11 +77,11 @@ public final class Clusters {
     }
 
     private static int backwardGlyphIndex(IntList clusterMap, int arrayIndex, int glyphCount) {
-        int leading = clusterMap.get(arrayIndex);
+        int common = clusterMap.get(arrayIndex);
 
         for (int i = arrayIndex - 1; i >= 0; i--) {
             int mapping = clusterMap.get(i);
-            if (mapping != leading) {
+            if (mapping != common) {
                 return mapping - 1;
             }
         }
@@ -62,12 +89,12 @@ public final class Clusters {
         return glyphCount - 1;
     }
 
-    public static int actualClusterStart(IntList clusterMap, int arrayIndex) {
-        int leading = clusterMap.get(arrayIndex);
+    public static int actualClusterStart(int[] clusterMap, int arrayIndex) {
+        int common = clusterMap[arrayIndex];
 
         for (int i = arrayIndex - 1; i >= 0; i--) {
-            int mapping = clusterMap.get(i);
-            if (mapping != leading) {
+            int mapping = clusterMap[i];
+            if (mapping != common) {
                 return i + 1;
             }
         }
@@ -75,13 +102,40 @@ public final class Clusters {
         return 0;
     }
 
+    public static int actualClusterStart(IntList clusterMap, int arrayIndex) {
+        int common = clusterMap.get(arrayIndex);
+
+        for (int i = arrayIndex - 1; i >= 0; i--) {
+            int mapping = clusterMap.get(i);
+            if (mapping != common) {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static int actualClusterEnd(int[] clusterMap, int arrayIndex) {
+        int common = clusterMap[arrayIndex];
+        int length = clusterMap.length;
+
+        for (int i = arrayIndex + 1; i < length; i++) {
+            int mapping = clusterMap[i];
+            if (mapping != common) {
+                return i;
+            }
+        }
+
+        return length;
+    }
+
     public static int actualClusterEnd(IntList clusterMap, int arrayIndex) {
-        int leading = clusterMap.get(arrayIndex);
+        int common = clusterMap.get(arrayIndex);
         int length = clusterMap.size();
 
         for (int i = arrayIndex + 1; i < length; i++) {
             int mapping = clusterMap.get(i);
-            if (mapping != leading) {
+            if (mapping != common) {
                 return i;
             }
         }
