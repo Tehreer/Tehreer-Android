b2e9fc2b7a382153b3f865afc9b520f08cbc6552
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-02-08 22:26:45 +0500

    [lib] Wrote abstract SFNT table class for common functionality

---
+++ tehreer-android/src/main/java/com/mta/tehreer/internal/sfnt/AbstractSfntTable.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017-2019 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.internal.sfnt;
+
+public abstract class AbstractSfntTable implements SfntTable {
+    protected AbstractSfntTable() { }
+
+    public float readFixed(int offset) {
+        return readInt32(offset) / 65536.0f;
+    }
+
+    public SfntTable subTable(int offset) {
+        return new SubTable(this, offset);
+    }
+}
