ac709974362a1065d9628dc46adebabf2f1140bd
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2019-02-10 17:41:28 +0500

    [demo] Handled font instance variation spinner

--- demo/src/main/java/com/mta/tehreer/demo/VariableFontsActivity.java
+++ demo/src/main/java/com/mta/tehreer/demo/VariableFontsActivity.java
@@ -16,11 +16,13 @@
 
 package com.mta.tehreer.demo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -39,12 +41,43 @@ import java.util.Collections;
 import java.util.List;
 
 public class VariableFontsActivity extends AppCompatActivity {
+    private Spinner mVariableInstanceSpinner;
     private TLabel mPreviewLabel;
+
+    private FontFile mFontFile;
     private Typeface mTypeface;
     private float[] mVariationCoordinates;
 
     private AxisAdapter mAxisAdapter = new AxisAdapter();
     private List<VariationAxis> mVariationAxes = Collections.emptyList();
+
+    private static class InstanceAdapter extends ArrayAdapter<Typeface> {
+        public InstanceAdapter(Context context, FontFile fontFile) {
+            super(context, android.R.layout.simple_spinner_item, fontFile.getTypefaces());
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        }
+
+        public String getName(int position) {
+            Typeface typeface = getItem(position);
+            return typeface.getStyleName();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = (TextView) super.getView(position, convertView, parent);
+            textView.setText(getName(position));
+
+            return textView;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            TextView textView = (TextView) super.getDropDownView(position, convertView, parent);
+            textView.setText(getName(position));
+
+            return textView;
+        }
+    }
 
     private class AxisAdapter extends BaseAdapter {
         @Override
@@ -86,16 +119,14 @@ public class VariableFontsActivity extends AppCompatActivity {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                     float newValue = mVariationAxes.get(position).minValue() + (i / 10.0f);
-                    onCoordinateValueChanged(position, newValue);
+                    setupCoordinate(position, newValue);
                 }
 
                 @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-                }
+                public void onStartTrackingTouch(SeekBar seekBar) { }
 
                 @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-                }
+                public void onStopTrackingTouch(SeekBar seekBar) { }
             });
 
             return convertView;
@@ -112,22 +143,28 @@ public class VariableFontsActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        mVariableInstanceSpinner = findViewById(R.id.spinner_variable_instance);
+        mVariableInstanceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                InstanceAdapter instanceAdapter = (InstanceAdapter) adapterView.getAdapter();
+                setupTypeface(instanceAdapter.getItem(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) { }
+        });
+
         Spinner variableFontSpinner = findViewById(R.id.spinner_variable_font);
         variableFontSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 VariableFontAdapter fontAdapter = (VariableFontAdapter) adapterView.getAdapter();
-                FontFile fontFile = fontAdapter.getFontFile(i);
-                mTypeface = fontFile.getTypefaces().get(0);
-                mVariationAxes = mTypeface.getVariationAxes();
-                mVariationCoordinates = mTypeface.getVariationCoordinates();
-                mAxisAdapter.notifyDataSetChanged();
-                mPreviewLabel.setTypeface(mTypeface);
+                setupFont(fontAdapter.getFontFile(i));
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
+            public void onNothingSelected(AdapterView<?> adapterView) { }
         });
         variableFontSpinner.setAdapter(new VariableFontAdapter(this));
         variableFontSpinner.setSelection(0);
@@ -144,9 +181,25 @@ public class VariableFontsActivity extends AppCompatActivity {
         return true;
     }
 
-    private void onCoordinateValueChanged(int axisIndex, float coordinate) {
-        mVariationCoordinates[axisIndex] = coordinate;
-        mTypeface = mTypeface.deriveVariation(mVariationCoordinates);
+    private void setupFont(FontFile fontFile) {
+        mFontFile = fontFile;
+        mTypeface = fontFile.getTypefaces().get(0);
+        mVariationAxes = mTypeface.getVariationAxes();
+        mVariationCoordinates = mTypeface.getVariationCoordinates();
+        mAxisAdapter.notifyDataSetChanged();
+        mVariableInstanceSpinner.setAdapter(new InstanceAdapter(this, fontFile));
         mPreviewLabel.setTypeface(mTypeface);
+    }
+
+    private void setupTypeface(Typeface typeface) {
+        mTypeface = typeface;
+        mVariationCoordinates = mTypeface.getVariationCoordinates();
+        mAxisAdapter.notifyDataSetChanged();
+        mPreviewLabel.setTypeface(mTypeface);
+    }
+
+    private void setupCoordinate(int axisIndex, float coordinate) {
+        mVariationCoordinates[axisIndex] = coordinate;
+        setupTypeface(mTypeface.deriveVariation(mVariationCoordinates));
     }
 }
