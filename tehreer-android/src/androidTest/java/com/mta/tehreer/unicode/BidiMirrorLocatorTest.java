6f87928630fad830da253754dc8e00929b74b7e6
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2022-03-20 13:45:18 +0500

    [test] Wrote test cases for bidi mirror locator

---
+++ tehreer-android/src/androidTest/java/com/mta/tehreer/unicode/BidiMirrorLocatorTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2022 Muhammad Tayyab Akram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mta.tehreer.unicode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BidiMirrorLocatorTest {
+    private static final String DEFAULT_TEXT = "یہ ایک (car) ہے۔";
+
+    private BidiLine bidiLine;
+    private BidiMirrorLocator sut;
+
+    @Before
+    public void setUp() {
+        String text = DEFAULT_TEXT;
+        BidiAlgorithm bidiAlgorithm = BidiAlgorithm.finalizable(
+            new BidiAlgorithm(text)
+        );
+        BidiParagraph bidiParagraph = BidiParagraph.finalizable(
+            bidiAlgorithm.createParagraph(0, text.length(), BaseDirection.DEFAULT_LEFT_TO_RIGHT)
+        );
+        bidiLine = BidiLine.finalizable(
+            bidiParagraph.createLine(0, text.length())
+        );
+        sut = new BidiMirrorLocator();
+    }
+
+    @After
+    public void tearDown() {
+        sut.dispose();
+    }
+
+    @Test
+    public void testNativePointers() {
+        assertNotEquals(sut.nativeMirrorLocator, 0);
+    }
+
+    @Test
+    public void testNextPair() {
+        sut.loadLine(bidiLine);
+
+        // For First Mirror
+        {
+            // When
+            BidiPair pair = sut.nextPair();
+
+            // Then
+            assertNotNull(pair);
+            assertEquals(pair.charIndex, 11);
+            assertEquals(pair.actualCodePoint, ')');
+            assertEquals(pair.pairingCodePoint, '(');
+        }
+
+        // For Last Mirror
+        {
+            // When
+            BidiPair pair = sut.nextPair();
+
+            // Then
+            assertNotNull(pair);
+            assertEquals(pair.charIndex, 7);
+            assertEquals(pair.actualCodePoint, '(');
+            assertEquals(pair.pairingCodePoint, ')');
+        }
+
+        // For No Available Mirror
+        {
+            // When
+            BidiPair pair = sut.nextPair();
+
+            // Then
+            assertNull(pair);
+        }
+    }
+}
