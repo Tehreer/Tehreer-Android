bdb5e8bd58464e5b3c5f9fb3e9c1e1e295dc7a0e
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2022-03-22 19:51:04 +0500

    [lib] Wrote `toString()` implementation in script run

--- tehreer-android/src/main/java/com/mta/tehreer/unicode/ScriptRun.java
+++ tehreer-android/src/main/java/com/mta/tehreer/unicode/ScriptRun.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Muhammad Tayyab Akram
+ * Copyright (C) 2018-2022 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,5 +77,13 @@ public class ScriptRun {
         result = 31 * result + script;
 
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ScriptRun{charStart=" + charStart
+                + ", charEnd=" + charEnd
+                + ", script=" + script
+                + '}';
     }
 }
